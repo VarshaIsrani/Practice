64dc829a64ca51ee11c893868ab6a63d8d96e8e9
unknown <varshai@varshai.cybage.com>
2016-08-16 19:30:03 +0530

    Practice Assignments

---
+++ first.js
@@ -0,0 +1,12 @@
+console.log("hi");
+setTimeout(function(){console.log("Time Out 2");},2);
+console.log("hi");
+console.log("hi"); 
+console.log("hi");
+console.log("hi");
+setTimeout(console.log("Time Out 7"),7);
+console.log("hi");
+console.log("hi");
+console.log("hi");
+console.log("hi");
+setTimeout(console.log("Time Out 3"),3);
